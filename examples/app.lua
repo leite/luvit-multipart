d58d9d5120c3c2c560cb66b4c2de267a5b197225
leite <xxleite@gmail.com>
2012-10-08 01:40:35 -0300

    add multipart and endpoint to test

--- examples/app.lua
+++ examples/app.lua
@@ -1,9 +1,22 @@
-local app = require('luvit-app'):new()
+process.env.DEBUG = '1'
+--local Log = require('lib/log')
+
+local app       = require('luvit-app'):new()
+local multipart = require '../multipart'
 
 local json_headers = {['Content-type'] = 'application/json'}
 
-app:GET('/', function(self, nxt)
+app:mount('/public/', 'static', {mount = '',root = __dirname .. '/public'})
+app:use(multipart())
+
+app:GET('/foo$', function(self, nxt)
+  p(self.req)
   self:send(200, '{"foo":"master"}', json_headers)
+end)
+
+app:POST('/foo$', function(self, nxt)
+  p(self.req)
+  self:send(200, '{"foo":"post_it"}', json_headers)
 end)
 
 app:run(6969, '0.0.0.0')
