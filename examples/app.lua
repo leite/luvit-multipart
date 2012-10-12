4a89ace50176eeb52e8f5136df597a57e77da9f1
leite <xxleite@gmail.com>
2012-10-11 22:43:29 -0300

    404 to favicon.ico

--- examples/app.lua
+++ examples/app.lua
@@ -1,6 +1,3 @@
-process.env.DEBUG = '1'
---local Log = require('lib/log')
-
 local app       = require('luvit-app'):new()
 local multipart = require '../multipart'
 
@@ -9,14 +6,13 @@ local json_headers = {['Content-type'] = 'application/json'}
 app:mount('/public/', 'static', {mount = '',root = __dirname .. '/public'})
 app:use(multipart())
 
-app:GET('/foo$', function(self, nxt)
-  p(self.req)
-  self:send(200, '{"foo":"master"}', json_headers)
-end)
-
 app:POST('/foo$', function(self, nxt)
   p(self.req)
   self:send(200, '{"foo":"post_it"}', json_headers)
+end)
+
+app:GET('/favicon.ico$', function(self, nxt)
+  self:send(404)
 end)
 
 app:run(6969, '0.0.0.0')
